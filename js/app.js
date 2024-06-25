f8def3914ba35c4a28bb164245d988b546c42cb3
Lucas Maximiliano Novo <lucasmaxnovo123@gmail.com>
2024-06-25 20:55:10 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso'); 
+    let qtdInput = document.getElementById('qtd');
+    let qtd = parseInt(qtdInput.value);
+
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert ('Quantidade insuficiente de ingressos!');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert ('Compra realizada com sucesso!')
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert ('Quantidade insuficiente de ingressos!');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert ('Compra realizada com sucesso!')
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert ('Quantidade insuficiente de ingressos!');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert ('Compra realizada com sucesso!')
+    }
+}
